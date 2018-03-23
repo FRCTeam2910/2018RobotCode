eec667e13d928dcb878c2a92c76d3a94def2ce7d
Jacob Bublitz <jacob@bublitz.com>
2018-03-22 20:02:25 -0700

    Make the elevator move back and forth when shifting into low gear

--- robot/src/main/java/org/usfirst/frc/team2910/robot/commands/ChangeElevatorModeCommand.java
+++ robot/src/main/java/org/usfirst/frc/team2910/robot/commands/ChangeElevatorModeCommand.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc.team2910.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team2910.robot.subsystems.ElevatorSubsystem;
 
 public class ChangeElevatorModeCommand extends Command {
+    private static final double WAIT_TIME = 0.75;
     private final ElevatorSubsystem elevator;
     private final ElevatorSubsystem.Mode targetMode;
+
+    private final Timer timer = new Timer();
 
     public ChangeElevatorModeCommand(ElevatorSubsystem elevator, ElevatorSubsystem.Mode targetMode) {
         this.elevator = elevator;
@@ -21,8 +25,20 @@ public class ChangeElevatorModeCommand extends Command {
     protected void initialize() {
         elevator.setElevatorSpeed(-0.1);
 
+        timer.reset();
+        timer.start();
+
         elevator.setMode(targetMode);
         elevator.setMode(targetMode);
+    }
+
+    @Override
+    protected void execute() {
+        elevator.setElevatorSpeed(-0.1);
+        if (timer.get() % WAIT_TIME > WAIT_TIME / 2)
+            elevator.setElevatorSpeed(-0.1);
+        else
+            elevator.setElevatorSpeed(0.1);
     }
 
     @Override
@@ -35,6 +51,10 @@ public class ChangeElevatorModeCommand extends Command {
 
     @Override
     protected void end() {
-        elevator.setElevatorPosition(elevator.getCurrentHeight());
+        timer.stop();
+        if (elevator.getCurrentMode() == ElevatorSubsystem.Mode.CLIMBING)
+            elevator.setElevatorSpeed(0);
+        else
+            elevator.setElevatorPosition(elevator.getCurrentHeight());
     }
 }
